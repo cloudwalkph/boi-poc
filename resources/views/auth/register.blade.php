59980af5da748d4c8aa51f833a2d9ea8a9dc814b
Nina Kristel Cheng <nkmcheng@live.com>
2017-03-30 13:04:27 +0800

    update register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,11 +2,23 @@
     <div class="row">
         <div class="col-md-12" style="z-index: 100; padding: 50px 80px;">
             <h2>Register</h2>
-                <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
+                <form class="" role="form" method="POST" action="{{ route('register') }}">
                     {{ csrf_field() }}
                     @include('components.errors')
 
                     <div class="col-md-12">
+                        <div class="form-group{{ $errors->has('profile.passport_number') ? ' has-error' : '' }}">
+                            <label for="profile[passport_number]" class="control-label">Passport Number</label>
+                            <input id="profile[passport_number]" type="text" class="form-control" name="profile[passport_number]" value="{{ old('profile.passport_number') }}" required>
+                            @if ($errors->has('profile.passport_number'))
+                                <span class="help-block">
+                                <strong>{{ $errors->first('profile.passport_number') }}</strong>
+                            </span>
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
                         <div class="form-group{{ $errors->has('profile.first_name') ? ' has-error' : '' }}">
                             <label for="profile[first_name]" class="control-label">First Name</label>
                             <input id="profile[first_name]" type="text" class="form-control" name="profile[first_name]" value="{{ old('profile.first_name') }}" required>
@@ -17,13 +29,25 @@
                             @endif
                         </div>
                     </div>
-                    <div class="col-md-12">
+                    <div class="col-md-6">
                         <div class="form-group{{ $errors->has('profile.last_name') ? ' has-error' : '' }}">
                             <label for="profile[last_name]" class="control-label">Last Name</label>
                             <input id="profile[last_name]" type="text" class="form-control" name="profile[last_name]" value="{{ old('profile.last_name') }}" required>
                             @if ($errors->has('profile.last_name'))
                                 <span class="help-block">
                                 <strong>{{ $errors->first('profile.last_name') }}</strong>
+                            </span>
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="col-md-12">
+                        <div class="form-group{{ $errors->has('profile.birthday') ? ' has-error' : '' }}">
+                            <label for="profile[birthday]" class="control-label">Birthday</label>
+                            <input id="profile[birthday]" type="date" class="form-control" name="profile[birthday]" value="{{ old('profile.birthday') }}" required>
+                            @if ($errors->has('profile.birthday'))
+                                <span class="help-block">
+                                <strong>{{ $errors->first('profile.birthday') }}</strong>
                             </span>
                             @endif
                         </div>
